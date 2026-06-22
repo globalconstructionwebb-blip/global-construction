66519f4ed938a14a51b1add90ee5135755828f03
Webix-se <info@webix.se>
2026-06-22 20:13:26 +0200

    chore: remove .html from urls

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    return [
-      { source: '/', destination: '/index.html' },
-      { source: '/om-oss', destination: '/om-oss.html' },
-      { source: '/konstruktion', destination: '/konstruktion.html' },
-      { source: '/skyddsrum', destination: '/skyddsrum.html' },
-      { source: '/projekt', destination: '/projekt.html' },
-      { source: '/blogg', destination: '/blogg.html' },
-      { source: '/kontakt', destination: '/kontakt.html' },
-      { source: '/faq', destination: '/faq.html' },
-      { source: '/jobba-hos-oss', destination: '/jobba-hos-oss.html' }
-    ];
+    return {
+      beforeFiles: [],
+      afterFiles: [
+        { source: '/', destination: '/index.html' }
+      ],
+      fallback: [
+        {
+          source: '/:path*',
+          destination: '/:path*.html',
+        },
+      ],
+    };
   },
 };
 
