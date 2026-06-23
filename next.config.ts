f3957f4deef55fd22f2db2c7315a66ece45cd45f
Webix-se <info@webix.se>
2026-06-23 22:21:17 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/iordningstallande-ritningar',
+        destination: '/iordningstallanderitningar',
+        permanent: true,
+      },
+    ];
+  },
   async rewrites() {
     return {
       beforeFiles: [],
